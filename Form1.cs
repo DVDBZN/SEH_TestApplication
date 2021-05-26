e4362daecec9e3c7b9f9eacc4511dbe174d13822
DVDBZN <davebozin@gmail.com>
2021-05-26 09:07:24 -0700

    Final update

--- Form1.cs
+++ Form1.cs
@@ -94,33 +94,36 @@ namespace SEH_TestApplication
             int wordCount = DescriptionTextBox.Text.Split(' ').Length;
             string[] words = DescriptionTextBox.Text.Split(' ');
 
+            //Iterates through all words and adds bolded ones to list
             for (int i = 0; i < wordCount; i++)
             {
                 DescriptionTextBox.Select(index, words[i].Length);
                 string temp = DescriptionTextBox.SelectedText;
 
                 if (DescriptionTextBox.SelectionFont.Style.ToString().Contains("Bold"))
-                {
                     bolded.Add(words[i]);
-                }
 
                 index += 1 + words[i].Length;
             }
 
+            //Allows API calls
             var pexelsClient = new PexelsClient("563492ad6f917000010000013a071a50d88f43419b2550f2a08eaced");
 
             try
             {
+                //Fetches 9 images based on title
                 var result = await pexelsClient.SearchPhotosAsync(TitleTextBox.Text, "", "", "", "", 1, 9);
 
                 for (int i = 0; i < result.photos.Count; i++)
                 {
+                    //Adds image URLs to lists
                     thumbnails.Add(result.photos[i].source.tiny);
                     ImageURLs.Add(result.photos[i].source.original);
                 }
 
                 foreach (string word in bolded)
                 {
+                    //Fetches 5 images for each bolded word and adds their URLs to lists
                     result = await pexelsClient.SearchPhotosAsync(word, "", "", "", "", 1, 5);
 
                     for (int i = 0; i < result.photos.Count; i++)
@@ -130,12 +133,14 @@ namespace SEH_TestApplication
                     }
                 }
             }
+            //In case text is not entered
             catch
             {
                 MessageBox.Show("Please enter a title and description.", "Missing text", MessageBoxButtons.OK);
                 return;
             }
 
+            //Shuffles both lists identically
             List<int> indexes = Enumerable.Range(0, thumbnails.Count()).ToList();
             Random rand = new Random();
             indexes = indexes.OrderBy(a => rand.Next()).ToList();
@@ -143,25 +148,24 @@ namespace SEH_TestApplication
             List<string> newImageURLs = indexes.Select(b => ImageURLs[b]).ToList();
             ImageURLs = newImageURLs;
 
+            //Loads 9 "random" images for display
             foreach (PictureBox box in imageBoxes)
-            {
                 box.Load(newThumbnails[imageBoxes.IndexOf(box)]);
-            }
         }
 
         private async void GenerateButton_Click(object sender, EventArgs e)
         {
             List<CheckBox> checkBoxes = new List<CheckBox> { checkBox1, checkBox2, checkBox3, checkBox4,
                 checkBox5, checkBox6, checkBox7, checkBox8, checkBox9 };
+            //Name for folder for downloaded images
             string folder = @"Resources\" + DateTime.Now.ToString("yyyy'-'MM'-'dd'-'HH'-'mm'-'ss");
 
             foreach (CheckBox box in checkBoxes)
             {
                 int i = checkBoxes.IndexOf(box);
+                //Downloads checked images
                 if (box.Checked)
-                {
                     await DownloadImageAsync(folder, "image" + i, new Uri(ImageURLs[i], UriKind.Absolute));
-                }
             }
 
             GenerateSlide();
@@ -171,15 +175,15 @@ namespace SEH_TestApplication
         {
             var httpClient = new HttpClient();
 
-            // Get the file extension
+            //Get the file extension
             var uriWithoutQuery = uri.GetLeftPart(UriPartial.Path);
             var fileExtension = Path.GetExtension(uriWithoutQuery);
 
-            // Create file path and ensure directory exists
+            //Create file path and ensure directory exists
             var path = Path.Combine(Application.StartupPath, directoryPath, $"{fileName}{fileExtension}");
             Directory.CreateDirectory(directoryPath);
 
-            // Download the image and write to the file
+            //Download the image and write to the file
             var imageBytes = await httpClient.GetByteArrayAsync(uri);
             File.WriteAllBytes(path, imageBytes);
 
@@ -188,13 +192,14 @@ namespace SEH_TestApplication
 
         private void GenerateSlide()
         {
+            //Create presentation file and slide
             Presentation presentation = new Presentation();
-
             ISlide slide = presentation.Slides[0];
 
             //Add content
             try
             {
+                //Add all downloaded images to presentation file
                 foreach (string path in ImagePaths)
                 {
                     int i = ImagePaths.IndexOf(path);
@@ -206,11 +211,13 @@ namespace SEH_TestApplication
                     presentation.Slides[0].Shapes.AddPictureFrame(ShapeType.Rectangle, (400 * (i + 1)) / ImagePaths.Count, (300 * (i + 1)) / ImagePaths.Count, 300, 200, img);
                 }
             }
+            //This may be unnecessary after recent bug fix
             catch
             {
                 return;
             }
 
+            //Create and format Title and Description texts
             IAutoShape shape = slide.Shapes.AddAutoShape(ShapeType.Rectangle, 250, 75, 300, 50);
             shape.ShapeStyle.LineColor.Color = Color.FromArgb(255, 255, 255, 255);
             shape.ShapeStyle.FillColor.Color = Color.FromArgb(200, 255, 255, 255);
@@ -231,11 +238,14 @@ namespace SEH_TestApplication
             IPortion portion2 = para2.Portions[0];
             portion2.Text = DescriptionTextBox.Text;
 
+            //Save presentation file
             presentation.Save(TitleTextBox.Text + ".pptx", Aspose.Slides.Export.SaveFormat.Pptx);
 
+            //Reset lists
             ImageURLs = new List<string> { };
             ImagePaths = new List<string> { };
 
+            //Confirmation
             MessageBox.Show("File located at " + Path.Combine(Application.StartupPath, TitleTextBox.Text + ".pptx"), "Slide Created!", MessageBoxButtons.OK);
         }
     }
